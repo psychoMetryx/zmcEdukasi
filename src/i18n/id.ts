8f0a22f1191eba24d6fc197f7c26ee7f7c90a21d
psychoMetryx <110074889+psychoMetryx@users.noreply.github.com>
2025-09-08 10:25:02 +0700

    feat: add clinic details to hero description

--- src/i18n/id.ts
+++ src/i18n/id.ts
@@ -2,9 +2,9 @@ export default {
   appTitle: 'Zihan Medical Center',
   appDescription: `#ZMCGakNyuekinKamu
 -- Sejak 2013--
-TANPA LIBUR
-Menerima BPJS Rawat Inap / Rawat Jalan
-Wanaraja, Garut`,
+• TANPA LIBUR
+• Menerima BPJS Rawat Inap / Rawat Jalan
+Wanaraja, Garut.`,
   home: 'Beranda',
   highContrast: 'Kontras Tinggi',
   largeText: 'Font Besar',
--- src/i18n/su.ts
+++ src/i18n/su.ts
@@ -1,10 +1,10 @@
 export default {
   appTitle: 'Zihan Medical Center',
   appDescription: `#ZMCGakNyuekinKamu
--- Sejak 2013--
-TANPA LIBUR
-Menerima BPJS Rawat Inap / Rawat Jalan
-Wanaraja, Garut`,
+-- Ti saprak 2013--
+• Tanpa Libur
+• Nampi BPJS Rawat Inap / Rawat Jalan
+Wanaraja, Garut.`,
   home: 'Tepas',
   highContrast: 'Kontras Luhur',
   largeText: 'Téks Gedé',
